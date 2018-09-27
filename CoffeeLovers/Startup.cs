f788fd18c56f3f696d26d5636d8c975d93d6f3d4
Ashwin Kumar H <ashwinkpes@hotmail.com>
2018-09-28 01:30:08 +0530

    Added method to list of registered services in start up

--- CoffeeLovers/Startup.cs
+++ CoffeeLovers/Startup.cs
@@ -7,11 +7,14 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Text;
 
 namespace CoffeeLovers
 {
     public class Startup
     {
+        private IServiceCollection _services;
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -73,6 +76,7 @@ namespace CoffeeLovers
             //Register Swagger
             services.AddSwaggerSettings(Configuration);
 
+            _services = services;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -81,6 +85,7 @@ namespace CoffeeLovers
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                ListAllRegisteredServices(app);
             }
             else
             {
@@ -100,6 +105,28 @@ namespace CoffeeLovers
             });
             app.SeedCoffeeContext(Configuration);
             app.UseSwaggerMiddleWare(Configuration);
+        }
+
+        private void ListAllRegisteredServices(IApplicationBuilder app)
+        {
+            app.Map("/allservices", builder => builder.Run(async context =>
+            {
+                var sb = new StringBuilder();
+                sb.Append("<h1>All Services</h1>");
+                sb.Append("<table><thead>");
+                sb.Append("<tr><th>Type</th><th>Lifetime</th><th>Instance</th></tr>");
+                sb.Append("</thead><tbody>");
+                foreach (var svc in _services)
+                {
+                    sb.Append("<tr>");
+                    sb.Append($"<td>{svc.ServiceType.FullName}</td>");
+                    sb.Append($"<td>{svc.Lifetime}</td>");
+                    sb.Append($"<td>{svc.ImplementationType?.FullName}</td>");
+                    sb.Append("</tr>");
+                }
+                sb.Append("</tbody></table>");
+                await context.Response.WriteAsync(sb.ToString());
+            }));
         }
     }
 }
