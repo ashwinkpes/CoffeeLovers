6d3e84f5625c289614d98b30ab647ae16e770d96
Ashwin Kumar H <ashwinkpes@hotmail.com>
2018-10-02 12:34:12 +0530

    Added mail functionality

---
+++ CoffeeLovers/Extensions/RegisterNotificationService.cs
@@ -0,0 +1,14 @@
+﻿using CoffeeLovers.NotificationService;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CoffeeLovers.Extensions
+{
+    internal static class RegisterNotificationService
+    {
+         internal static void AddNotificationService(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddTransient<IAzureEmailSender>(s => new AzureEmailSender(new AzureEmailSettings(configuration["SendGridSettings:ApiKey"])));
+        }
+    }
+}
---
+++ CoffeeLovers/Helpers/Mail.cs
@@ -0,0 +1,21 @@
+﻿using CoffeeLovers.NotificationService;
+using CoffeeLovers.NotificationService.Message;
+using System.Threading.Tasks;
+
+namespace CoffeeLovers.Helpers
+{
+    internal class Mail
+    {
+        private readonly IAzureEmailSender _azureEmailSender;
+
+        Mail(IAzureEmailSender azureEmailSender)
+        {
+            _azureEmailSender = azureEmailSender;
+        }
+
+        internal async Task<ResponseMessage> SendMailAsync(EmailMessage message)
+        {
+           return await _azureEmailSender.SendAsync(message);
+        }
+    }
+}
